8835f7b1a84d4b8df3d624497159ab3fd9d223bd
Pendentyw <pal7fal7@gmail.com>
2023-08-02 18:21:38 +0200

    increment, decrement, and customized value inc

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,18 +1,58 @@
-import useCounter from '../hooks/use-counter';
+import { useState } from 'react';
 import Button from '../components/Button';
+import Panel from '../components/Panel';
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value) || 0;
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
 
   return (
-    <div>
-      <h1>count is {count}</h1>
-      <Button
-        onClick={increment}
-        outline>
-        Increment
-      </Button>
-    </div>
+    <Panel className='m-3 flex flex-col'>
+      <h1 className='text-lg'>Count is {count}</h1>
+      <div className='flex flex-row'>
+        <Button
+          onClick={increment}
+          outline>
+          Increment
+        </Button>
+        <Button
+          onClick={decrement}
+          outline>
+          Decrement
+        </Button>
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className=' flex flex-col items-start'>
+        <label className='mt-2'>Add a lot!</label>
+        <input
+          value={valueToAdd || ''}
+          onChange={handleChange}
+          className=' p-1 border bg-gray-50 border-gray-200 mb-2'
+          type='number'
+        />
+        <Button outline>Add</Button>
+      </form>
+    </Panel>
   );
 }
 

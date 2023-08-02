3bdfc210f0da3d2ffd6b596ccce354a72a2ba099
Pendentyw <pal7fal7@gmail.com>
2023-08-02 17:04:42 +0200

    counter logic

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,7 +1,27 @@
 import { useState, useEffect } from 'react';
+import Button from '../components/Button';
 
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>count is {count}</h1>
+      <Button
+        onClick={handleClick}
+        outline>
+        Increment
+      </Button>
+    </div>
+  );
 }
 
 export default CounterPage;

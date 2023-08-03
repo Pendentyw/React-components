75bafb2e2c698d852b5b18bce50997cda744390c
Pendentyw <pal7fal7@gmail.com>
2023-08-03 15:05:36 +0200

    changed useState to useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,33 +1,69 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const reducer = (state, action) => {
+  if (action.type === 'increment') {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      ...state,
+      count: state.count - 1,
+    };
+  }
+
+  if (action.type === 'change-value') {
+    return {
+      ...state,
+      valueToAdd: action.payload,
+    };
+  }
+
+  if (action.type === 'add-value') {
+    return {
+      ...state,
+      count: state.count + state.valueToAdd,
+      valueToAdd: 0,
+    };
+  }
+
+  return state;
+};
+
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: initialCount, valueToAdd: 0 });
 
   const increment = () => {
-    setCount(count + 1);
+    dispatch({ type: 'increment' });
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    dispatch({ type: 'decrement' });
   };
 
   const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0;
-    setValueToAdd(value);
+    dispatch({
+      type: 'change-value',
+      payload: value,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    dispatch({
+      type: 'add-value',
+    });
   };
 
   return (
     <Panel className='m-3 flex flex-col'>
-      <h1 className='text-lg'>Count is {count}</h1>
+      <h1 className='text-lg'>Count is {state.count}</h1>
       <div className='flex flex-row'>
         <Button
           onClick={increment}
@@ -43,9 +79,9 @@ function CounterPage({ initialCount }) {
       <form
         onSubmit={handleSubmit}
         className=' flex flex-col items-start'>
-        <label className='mt-2'>Add a lot!</label>
+        <label className='mt-2'>Add more!</label>
         <input
-          value={valueToAdd || ''}
+          value={state.valueToAdd || ''}
           onChange={handleChange}
           className=' p-1 border bg-gray-50 border-gray-200 mb-2'
           type='number'
